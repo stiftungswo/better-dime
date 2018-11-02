5417b054bd15e5c5394e1ce6ad25ac63bc2da602
Andy Pfister <andy.pfister@hotmail.ch>
2018-11-02 14:59:09 +0100

    execute ci phpunit tests in random order

--- ci/test_backend.sh
+++ ci/test_backend.sh
@@ -4,7 +4,7 @@ if [[ $TRAVIS_COMMIT_MESSAGE != *"[skip-tests]"* ]]; then
     cp ./api/.env.travis ./api/.env
     php api/artisan db:create
     php api/artisan migrate:fresh -q
-    ./api/vendor/phpunit/phpunit/phpunit -c api --coverage-clover=coverage.xml
+    ./api/vendor/phpunit/phpunit/phpunit --order-by=random -c api --coverage-clover=coverage.xml
     if [ $? == 0 ]; then
         bash <(curl -s https://codecov.io/bash)
     fi
