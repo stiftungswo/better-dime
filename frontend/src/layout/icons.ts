6ebf7b01250bd81d78e18efa82c58283dc72c703
tourn <latzer.daniel@gmail.com>
2018-11-21 10:13:36 +0100

    Extract icons WIP

---
+++ frontend/src/layout/icons.ts
@@ -0,0 +1,8 @@
+/*
+If your environment support tree-shaking you can also import the icons this way:
+
+    import { AccessAlarm, ThreeDRotation } from '@material-ui/icons';
+
+Note: Importing named exports in this way will result in the code for every icon being included in your project,
+so is not recommended unless you configure tree-shaking. It may also impact Hot Module Reload performance.
+ */
