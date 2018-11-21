3da00c3d73f16a3e6b8d1032fe7925330c367ec4
tourn <latzer.daniel@gmail.com>
2018-11-21 13:44:49 +0100

    Display validation errors in DatePicker

--- frontend/src/form/fields/DatePicker.tsx
+++ frontend/src/form/fields/DatePicker.tsx
@@ -3,20 +3,26 @@ import { ReactNode } from 'react';
 import { DatePicker as MUIDatePicker } from 'material-ui-pickers';
 import { Moment } from 'moment';
 import { FormProps } from './common';
+import { getIn } from 'formik';
 
-//TODO: Display validation errors https://github.com/dmtrKovalenko/material-ui-pickers/issues/761
-export const DatePicker = ({ field, form, required, ...rest }: FormProps & { children: ReactNode }) => (
-  <MUIDatePicker
-    keyboard
-    autoOk
-    format="DD.MM.YYYY"
-    placeholder="10.10.2018"
-    mask={(value: any) => (value ? [/\d/, /\d/, '.', /\d/, /\d/, '.', /\d/, /\d/, /\d/, /\d/] : [])}
-    value={field.value ? new Date(field.value) : field.value}
-    onChange={(date?: Moment) => form.setFieldValue(field.name, date ? date.format('YYYY-MM-DD') : date)}
-    disableOpenOnEnter
-    animateYearScrolling={false}
-    clearable={!required}
-    {...rest}
-  />
-);
+export const DatePicker = ({ field, form, required, ...rest }: FormProps & { children: ReactNode }) => {
+  const currentError = getIn(form.errors, field.name);
+  return (
+    <MUIDatePicker
+      keyboard
+      autoOk
+      format="DD.MM.YYYY"
+      placeholder="10.10.2018"
+      mask={(value: any) => (value ? [/\d/, /\d/, '.', /\d/, /\d/, '.', /\d/, /\d/, /\d/, /\d/] : [])}
+      value={field.value ? new Date(field.value) : field.value}
+      onChange={(date?: Moment) => form.setFieldValue(field.name, date ? date.format('YYYY-MM-DD') : date)}
+      disableOpenOnEnter
+      animateYearScrolling={false}
+      clearable={!required}
+      error={!!currentError}
+      helperText={currentError}
+      onError={(_, error: any) => form.setFieldError(field.name, error)}
+      {...rest}
+    />
+  );
+};
