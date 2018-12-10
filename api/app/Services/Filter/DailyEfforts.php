12a9bcbeb2a6ae5d8c664b0395ee04c3e444369f
Andy Pfister <andy.pfister@hotmail.ch>
2018-12-10 15:53:46 +0100

    Excluded Deleted Efforts in DailyEffortFilter

--- api/app/Services/Filter/DailyEfforts.php
+++ api/app/Services/Filter/DailyEfforts.php
@@ -16,7 +16,10 @@ class DailyEfforts
             ->join('services', 'project_positions.service_id', '=', 'services.id')
             ->join('rate_units', 'project_positions.rate_unit_id', '=', 'rate_units.id')
             ->select('employee_id', 'first_name', 'last_name', 'date', 'project_efforts.value', 'service_id', 'services.name as service_name')
-            ->where('rate_units.is_time', 'is', true)
+            ->where([
+                ['rate_units.is_time', '=', true],
+                ['project_efforts.deleted_at', '=', null]
+            ])
             ->whereBetween('project_efforts.date', [$from->startOfDay(), $to->endOfDay()])
             ->orderBy('date')
             ->get();
