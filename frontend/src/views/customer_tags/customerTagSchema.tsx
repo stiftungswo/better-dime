13d003af8f702dbf5fce978c237e8e744dcb32c2
Andy Pfister <andy.pfister@hotmail.ch>
2018-12-11 13:41:15 +0100

    Added Archived Flag to Default Attributes of CustomerTag

--- frontend/src/views/customer_tags/customerTagSchema.tsx
+++ frontend/src/views/customer_tags/customerTagSchema.tsx
@@ -1,8 +1,10 @@
 import * as yup from 'yup';
 
 export const customerTagSchema = yup.object({
+  archived: yup.boolean(),
   name: yup.string().required(),
 });
 export const customerTagTemplate = {
+  archived: false,
   name: '',
 };
