59e4a1cfba0bf30445cb07201cda9c68de15a7b2
Andy Pfister <andy.pfister@hotmail.ch>
2018-12-13 10:18:48 +0100

    explicit cast input in sum function to Number

--- frontend/src/utilities/helpers.ts
+++ frontend/src/utilities/helpers.ts
@@ -11,7 +11,7 @@ export const empty = (value: any) => {
   return false;
 };
 
-export const sum = (arr: number[]) => arr.reduce((a, b) => a + b, 0);
+export const sum = (arr: number[]) => arr.reduce((a, b) => Number(a) + Number(b), 0);
 
 // tslint:disable-next-line:no-any
 export const captureException = (e: any) => Sentry.captureException(e);
