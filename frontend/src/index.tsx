11ca13124f987bbae9a2a8ceb5187996273c098b
tourn <latzer.daniel@gmail.com>
2019-01-16 15:04:50 +0100

    Disable service worker
    We weren't really profiting from it and the need to double reload
    confused users.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,7 +4,6 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import registerServiceWorker from './utilities/registerServiceWorker';
 import { createBrowserHistory } from 'history';
 import { Router } from 'react-router-dom';
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
@@ -38,4 +37,3 @@ ReactDOM.render(
   </StoreProvider>,
   document.getElementById('root') as HTMLElement
 );
-registerServiceWorker();
