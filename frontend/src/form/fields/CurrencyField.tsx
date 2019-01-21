6b51ea0f36cc7e1f6941b5576d72fc8dbe83e7de
tourn <latzer.daniel@gmail.com>
2019-01-21 15:38:34 +0100

    Fix CurrencyField returning non-integer values

--- frontend/src/form/fields/CurrencyField.tsx
+++ frontend/src/form/fields/CurrencyField.tsx
@@ -3,9 +3,8 @@ import { TransformingField, TransformingFieldProps } from './TransformingField';
 
 const factor = 100;
 const toString = (n: number) => (n / factor).toFixed(2);
-const toValue = (s: string) => Number(s) * factor;
+const toValue = (s: string) => Math.round(Number(s) * factor);
 
-//FIXME this field should always return integers, but doesn't if you enter a value like "120.1212"
 const CurrencyField = (props: TransformingFieldProps<number>) => (
   <TransformingField {...props} toString={toString} toValue={toValue} type={'number'} unit={'CHF'} />
 );
