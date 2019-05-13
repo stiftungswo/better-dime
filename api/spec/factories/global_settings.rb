2d6b474e864f3ec6ef253f8bc494639804499da1
Lukas Bischof <polinderis@gmail.com>
2019-05-13 13:32:53 +0200

    GlobalSetting model

---
+++ api/spec/factories/global_settings.rb
@@ -0,0 +1,17 @@
+FactoryBot.define do
+  factory :global_setting do
+    sender_name { "MyString" }
+    sender_street { "MyString" }
+    sender_zip { "" }
+    sender_phone { "MyString" }
+    sender_city { "MyString" }
+    sender_mail { "MyString" }
+    sender_vat { "MyString" }
+    sender_bank { "MyString" }
+    sender_web { "MyString" }
+    service_order_comment { "MyString" }
+    sender_bank_detail { "MyString" }
+    sender_bank_iban { "MyString" }
+    sender_bank_bic { "MyString" }
+  end
+end
---
+++ api/spec/models/global_setting_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe GlobalSetting, type: :model do
+  it { is_expected.to validate_presence_of :sender_name }
+  it { is_expected.to validate_presence_of :sender_street }
+  it { is_expected.to validate_presence_of :sender_zip }
+  it { is_expected.to validate_presence_of :sender_phone }
+  it { is_expected.to validate_presence_of :sender_city }
+  it { is_expected.to validate_presence_of :sender_mail }
+  it { is_expected.to validate_presence_of :sender_vat }
+  it { is_expected.to validate_presence_of :sender_bank }
+  it { is_expected.to validate_presence_of :sender_web }
+  it { is_expected.to validate_presence_of :service_order_comment }
+  it { is_expected.to validate_presence_of :sender_bank_detail }
+  it { is_expected.to validate_presence_of :sender_bank_iban }
+  it { is_expected.to validate_presence_of :sender_bank_bic }
+end
