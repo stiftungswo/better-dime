d5840c82d96eabaec8ecd4919f3fc15099e9dfe6
Niels Saurer <me@nielssaurer.com>
2019-05-16 09:21:47 +0200

    Add TODO for holiday date validation spec

--- api/spec/models/holiday_spec.rb
+++ api/spec/models/holiday_spec.rb
@@ -7,4 +7,6 @@ RSpec.describe Holiday, type: :model do
   it { is_expected.to validate_presence_of :date }
   it { is_expected.to validate_presence_of :name }
   it { is_expected.to validate_numericality_of(:duration).only_integer.is_greater_than(0) }
+
+  # TODO: add date validation spec
 end
