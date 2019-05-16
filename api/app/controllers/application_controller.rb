26bc65ca1c6b9695cbda41f92d4d121241ad084c
Niels Saurer <me@nielssaurer.com>
2019-05-16 13:26:14 +0200

    Add paper-trail (#79)

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 # class ApplicationController < ActionController::API
+# TODO: before_action :set_paper_trail_whodunnit  (https://github.com/paper-trail-gem/paper_trail#1b-installation)
 # end
