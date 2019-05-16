e31322cf013b530c3d870d2919b87c3f488f272d
Niels Saurer <me@nielssaurer.com>
2019-05-16 09:20:53 +0200

    Validate date on holiday

--- api/app/models/holiday.rb
+++ api/app/models/holiday.rb
@@ -3,4 +3,6 @@
 class Holiday < ApplicationRecord
   validates :date, :duration, :name, presence: true
   validates :duration, numericality: { greater_than: 0, only_integer: true }
+
+  validates :date, timeliness: { format: :date }
 end
