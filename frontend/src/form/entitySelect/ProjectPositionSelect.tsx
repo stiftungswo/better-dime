ecc02bc3020dcac4c5f4fa9a58695afdfc3bbaba
gumbee <mugeeb.hassan@gmail.com>
2019-12-04 10:08:24 +0100

    have a unified layout between invoice and offer but keep old layout for ungrouped projects and show group in timetrack

--- frontend/src/form/entitySelect/ProjectPositionSelect.tsx
+++ frontend/src/form/entitySelect/ProjectPositionSelect.tsx
@@ -2,7 +2,9 @@ import { inject, observer } from 'mobx-react';
 import React from 'react';
 import { EffortStore } from '../../stores/effortStore';
 import { ProjectStore } from '../../stores/projectStore';
+import {PositionGroup} from '../../types';
 import compose from '../../utilities/compose';
+import {defaultPositionGroup} from '../../utilities/helpers';
 import { DimeCustomFieldProps } from '../fields/common';
 import Select from '../fields/Select';
 
@@ -20,10 +22,15 @@ export class ProjectPositionSelect extends React.Component<Props> {
 
   get options() {
     if (this.props.effortStore!.selectedProject) {
-      return this.props.effortStore!.selectedProject!.positions.map(e => ({
-        value: e.id,
-        label: e.service.name,
-      }));
+      const groups = [defaultPositionGroup(), ...this.props.effortStore!.selectedProject!.position_groupings];
+      return this.props.effortStore!.selectedProject!.positions.map(e => {
+        const group = groups.find((g: PositionGroup) => g.id === e.position_group_id);
+        const groupName = group != null && groups.length > 1 ? ' [' + group.name + ']' : '';
+        return {
+          value: e.id,
+          label: e.service.name + groupName,
+        };
+      });
     } else {
       return [];
     }
