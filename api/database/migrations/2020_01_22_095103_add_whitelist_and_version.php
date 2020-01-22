beb930f3001852ea0587ec4caf13566e7d087851
gumbee <mugeeb.hassan@gmail.com>
2020-01-22 11:43:13 +0100

    add whitelisted jwt and version tables to php backend

---
+++ api/database/migrations/2020_01_22_095103_add_whitelist_and_version.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddWhitelistAndVersion extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('whitelisted_jwts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('jti');
+            $table->string('aud')->nullable;
+            $table->dateTime('exp');
+            $table->unsignedInteger('employee_id')->nullable();
+            $table->foreign('employee_id')->references('id')->on('employees')->onDelete('set null');
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+        });
+
+        Schema::create('versions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('item_type', 191);
+            $table->bigInteger('item_id');
+            $table->string('event');
+            $table->string('whodunnit')->nullable();
+            $table->longText('object')->nullable();
+            $table->timestamp('created_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('whitelisted_jwts');
+        Schema::dropIfExists('versions');
+    }
+}
