a84d77a9abaa8c712ec6915c7b2f7af797164f9a
Fabian Stillhart <fabian.stillhart1@swisscom.com>
2020-01-28 14:59:57 +0100

    ensure single table inheritance is compatible with php's lower case type names

--- apir/app/models/customer.rb
+++ apir/app/models/customer.rb
@@ -11,7 +11,32 @@ class Customer < ApplicationRecord
   has_many :offers, dependent: :restrict_with_exception
   has_many :projects, dependent: :restrict_with_exception
 
-  validates :type, inclusion: %w[Person Company]
+  validates :type, inclusion: %w[Person Company person company]
 
   alias phone_numbers phones
+
+  private
+    # PHP single table inheritance is inherently incomaptible with rails single table inheritance
+    # PHP requires the type content to be lowercase
+    # Rails requires the type content to be capitalized
+    # For the desired temporary intercompatibility we need to support lower case type names in rails
+    # customer.type: company -> rails won't find the class Company
+    # customer.type: company -> capitalize -> Company -> rails find the class Company
+    # This source code might change on every Rails release. This is a private undocumented rails method. 
+    # https://github.com/rails/rails/blob/b305f0e206b0d15ea81f9571669d9e831c9193e0/activerecord/lib/active_record/inheritance.rb#L251
+    warn "Customer.find_sti_class <private> might has change since rails '6.0.2.1', please ensure it still works" unless Gem.loaded_specs["rails"].version == "6.0.2.1"
+    def self.find_sti_class(type_name)
+      type_name = type_name.capitalize
+      super
+      ## .find_sti_class content as of 6.0.2.1
+      # type_name = base_class.type_for_attribute(inheritance_column).cast(type_name)
+      # subclass = self.sti_class_for(type_name)
+
+      # unless subclass == self || descendants.include?(subclass)
+      #   raise SubclassNotFound, "Invalid single-table inheritance type: #{subclass.name} is not a subclass of #{name}"
+      # end
+
+      # subclass
+    end
+
 end
