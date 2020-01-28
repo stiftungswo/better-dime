921bc18b66119407b7a38f45e3d2dc6990912352
gumbee <mugeeb.hassan@gmail.com>
2020-01-28 11:32:02 +0100

    use ransack to search offers

--- apir/app/controllers/v2/offers_controller.rb
+++ apir/app/controllers/v2/offers_controller.rb
@@ -2,18 +2,22 @@ module V2
   class OffersController < APIController
     def index
       @q = Offer.order(id: :desc).ransack(search_params)
-      @offers = @q.result.page(params[:page]).per(params[:pageSize])
+      @offers = @q.result.page(legacy_params[:page]).per(legacy_params[:pageSize])
     end
 
     def show
       @offer = Offer.find(params[:id])
     end
 
+    def legacy_params
+      params.permit(:orderByTag, :orderByDir,:showArchived,:filterSearch, :page, :pageSize)
+    end
+
     def search_params
       search = params.fetch(:q, {}).permit!
-      search[:s] ||= "#{params[:orderByTag]} #{params[:orderByDir]}"
-      search[:id_or_name_or_description_or_short_description] ||= params[:filterSearch]
-      search
+      search[:s] ||= "#{legacy_params[:orderByTag]} #{legacy_params[:orderByDir]}"
+      search[:id_or_name_or_description_or_short_description_cont] ||= legacy_params[:filterSearch]
+      search.permit(:s, :id_or_name_or_description_or_short_description_cont)
     end
   end
 end
--- apir/app/models/application_record.rb
+++ apir/app/models/application_record.rb
@@ -4,4 +4,12 @@ class ApplicationRecord < ActiveRecord::Base
   include Discard::Model
   self.abstract_class = true
   has_paper_trail
+
+  def self.inherited(subclass)
+    super
+
+    subclass.ransacker :id, type: :string do
+      Arel.sql("#{subclass.table_name}.`id`")
+    end
+  end
 end
