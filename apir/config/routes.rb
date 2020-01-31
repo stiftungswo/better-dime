bebccd1b384f425212242fe4356a8b1f62c8728f
gumbee <mugeeb.hassan@gmail.com>
2020-01-31 11:59:03 +0100

    simplify routes

--- apir/config/routes.rb
+++ apir/config/routes.rb
@@ -8,13 +8,10 @@ Rails.application.routes.draw do
       post 'duplicate', on: :member
     end
 
-    get 'projects', to: 'projects#index'
-    get 'projects/:id', to: 'projects#show', constraints: { id: /[0-9]+/ }
-    get 'projects/potential_invoices', action: :potential_invoices, controller: 'projects'
-    put 'projects/:id', to: 'projects#update', constraints: { id: /[0-9]+/ }
-    post 'projects/', to: 'projects#create'
-    post 'projects/:id/duplicate', to: 'projects#duplicate', constraints: { id: /[0-9]+/ }
-    delete 'projects/:id', to: 'projects#destroy', constraints: { id: /[0-9]+/ }
+    resources :projects, constraints: { id: /[0-9]+/ } do
+      post 'duplicate', on: :member
+      get 'potential_invoices', on: :collection, action: :potential_invoices, controller: 'projects'
+    end
 
     resources :invoices do
       post 'duplicate', on: :member
