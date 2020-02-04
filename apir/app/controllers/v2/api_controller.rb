59c0b5039218829d844c02ea74875d5ee92be72f
Fabian Stillhart <fabian.stillhart1@swisscom.com>
2020-02-04 10:34:24 +0100

    authenticate user unless dev env for rails api - for ease of development

--- apir/app/controllers/v2/api_controller.rb
+++ apir/app/controllers/v2/api_controller.rb
@@ -2,6 +2,7 @@
 
 module V2
   class APIController < ApplicationController
+    before_action :authenticate_user! unless Rails.env.development?
     helper ApplicationHelper
     helper Ransack::Helpers::FormHelper
     helper Kaminari::Helpers
