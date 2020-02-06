3038264075f8bd2bf7bacecd82d07d37c9a44747
gumbee <mugeeb.hassan@gmail.com>
2020-02-06 09:32:19 +0100

    improve project efforts performance and fix some specs

--- apir/spec/models/project_category_spec.rb
+++ apir/spec/models/project_category_spec.rb
@@ -4,6 +4,5 @@ require "rails_helper"
 
 RSpec.describe ProjectCategory, type: :model do
   it { is_expected.to validate_presence_of :name }
-  it { is_expected.to validate_presence_of :archived }
   it { is_expected.to have_many(:projects).dependent :restrict_with_exception }
 end
