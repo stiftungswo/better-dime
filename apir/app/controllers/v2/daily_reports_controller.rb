7750c87d407c7a3fe2ab29c1fe86ed4bfaa2a224
gumbee <mugeeb.hassan@gmail.com>
2020-02-26 09:56:01 +0100

    improve performance a bit more

--- apir/app/controllers/v2/daily_reports_controller.rb
+++ apir/app/controllers/v2/daily_reports_controller.rb
@@ -5,28 +5,23 @@ module V2
       to_date = DateTime.parse(params[:to]) || DateTime.now()
 
       @dates = (from_date..to_date)
-      @employee_efforts = calculate_efforts ProjectEffort.includes(:employee, project_position: [:service]).where(date: @dates), @dates
+      @employee_efforts = calculate_efforts ProjectEffort.includes(:employee, project_position: [:service]).where(date: @dates)
     end
 
-    def calculate_efforts(efforts, dates)
+    def calculate_efforts(efforts)
       employees = efforts.map {|effort| effort.employee}.uniq
-      employee_efforts = []
-      employees.each do |employee|
-        employee_efforts.push({
+      employees.map do |employee|
+        ({
           employee_id: employee.id,
           name: employee.full_name,
-          efforts: dates.map do |date|
-            [date.strftime('%F'), get_efforts_for_employee_date(efforts, employee, date)]
-          end.select {|t| t[1].length > 0}.to_h
+          efforts: get_efforts_for_employee(efforts, employee).group_by {|e| e[:date]},
         })
       end
-
-      employee_efforts
     end
 
-    def get_efforts_for_employee_date(efforts, employee, date)
-      efforts.select {|e| e.employee == employee && e.date == date}.map do |e| ({
-        date: date.strftime('%F'),
+    def get_efforts_for_employee(efforts, employee)
+      efforts.select {|e| e.employee == employee}.map do |e| ({
+        date: e.date.strftime('%F'),
         value: e.value,
         service_id: e.project_position.service.id,
         service_name: e.project_position.service.name
