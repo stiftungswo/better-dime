d70d7343fd5f1b9969787378d4b47d13d69464ac
Fabian Stillhart <fabian.stillhart1@swisscom.com>
2020-02-28 11:46:02 +0100

    make the code slightly prettier

--- apir/app/services/revenue_report_service.rb
+++ apir/app/services/revenue_report_service.rb
@@ -24,23 +24,27 @@ class RevenueReportService
 
   def rows
     offers.map do |offer|
-      offer_price = ((offer.breakdown[:fixed_price] || offer.breakdown[:total]) / 100).round # TODO Decorator
-      ["Offerte", offer.name, nil, offer.customer&.name, offer.created_at.strftime("%d.%m.%Y"), offer.accountant&.name, nil, nil, offer_price] + 
-        cost_groups.map {nil}
+      offer_price = (offer.breakdown[:fixed_price] || offer.breakdown[:total]).round # TODO Decorator
+      row = ["Offerte", offer.name, nil, offer.customer&.name, offer.created_at.strftime("%d.%m.%Y"), offer.accountant&.name, nil, nil, offer_price]
+      row += cost_groups.map {nil}
+      row = row.map {|column| column.is_a?(Numeric) ? (column / 100).round : column}
+      row 
     end + projects.map do |project|
-      offer_price = project.offer ? ((project.offer.breakdown[:fixed_price] || project.offer.breakdown[:total]) / 100).round : nil # TODO Decorator
-      invoice_price = project.invoices.sum {|invoice| ((invoice.breakdown[:fixed_price] || invoice.breakdown[:total]) / 100).round } # TODO Decorator
+      offer_price = project.offer ? (project.offer.breakdown[:fixed_price] || project.offer.breakdown[:total]) : nil # TODO Decorator
+      invoice_price = project.invoices.sum {|invoice| (invoice.breakdown[:fixed_price] || invoice.breakdown[:total]) } # TODO Decorator
       invoice_price = nil if invoice_price.zero?
       invoice_price_by_costgroup = {}
       project.invoices.each do |invoice|
         invoice.invoice_costgroup_distributions.each do |distribution|
           invoice_price_by_costgroup[distribution.costgroup_number] ||= 0
-          invoice_price_by_costgroup[distribution.costgroup_number] += ((invoice.breakdown[:fixed_price] || invoice.breakdown[:total]) / 100 * (distribution.weight / 100)).round
+          invoice_price_by_costgroup[distribution.costgroup_number] += ((invoice.breakdown[:fixed_price] || invoice.breakdown[:total]) * (distribution.weight / 100)).round
         end
       end
       current_price = nil # (project.current_price  / 100).round # TODO maybe the right value? # TODO THIS IS SLOW AS FUCK N+X
-      ["Projekt", project.name, project.project_category&.name, project.customer&.name, project.created_at.strftime("%d.%m.%Y"), project.accountant&.name, current_price, invoice_price, offer_price] + 
-        cost_groups.map {|cost_group| invoice_price_by_costgroup[cost_group.number]}
+      row = ["Projekt", project.name, project.project_category&.name, project.customer&.name, project.created_at.strftime("%d.%m.%Y"), project.accountant&.name, current_price, invoice_price, offer_price]
+      row += cost_groups.map {|cost_group| invoice_price_by_costgroup[cost_group.number]}
+      row = row.map {|column| column.is_a?(Numeric) ? (column / 100).round : column}
+      row 
     end
   end
 
