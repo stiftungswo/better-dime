4e8767f36e43315a59d396ada8fa867b8e7a9c94
gumbee <mugeeb.hassan@gmail.com>
2020-05-27 11:27:36 +0200

    split effort weights based on project cost group distribution in cost group report

--- apir/app/services/cost_group_report_service.rb
+++ apir/app/services/cost_group_report_service.rb
@@ -2,20 +2,50 @@
 
 class CostGroupReportService
   attr_accessor :daterange
-  attr_accessor :employees, :project_efforts, :project_positions, :effort_minutes, :effort_minutes_by_cost_group, :cost_groups
+  attr_accessor :employees, :projects, :project_efforts, :project_positions, :efforts_by_project, :effort_minutes_weighted, :cost_groups
 
   def initialize(daterange = (Date.today.beginning_of_year..Date.today.end_of_year), employee_group_name: "SWO Angestellte")
     self.daterange = daterange
-    self.project_positions = ProjectPosition.joins(:rate_unit, project: :costgroups, project_efforts: { employee: :employee_group }).where("rate_units.is_time" => true, "project_efforts.date" => daterange).where("employee_groups.name" => employee_group_name)
+    self.project_positions = ProjectPosition.joins(:project, :rate_unit, project: :costgroups, project_efforts: { employee: :employee_group })
+                               .where("rate_units.is_time" => true, "project_efforts.date" => daterange)
+                               .where("employee_groups.name" => employee_group_name)
+                               #.where(:project => Project.where(vacation_project: 0)) Uncomment if vacation projects should not be considered
+    self.projects = Project.where(id: project_positions.select("project_id"))
     self.employees = Employee.where(id: project_positions.select("employees.id")).order(first_name: :asc, last_name: :asc)
     self.cost_groups = Costgroup.where(number: project_positions.select("costgroups.number")).order(number: :asc, name: :asc)
-    self.effort_minutes = project_positions.group("employees.id", "costgroups.number").sum("project_efforts.value")
-    self.effort_minutes_by_cost_group = project_positions.group("costgroups.number").sum("project_efforts.value")
+    self.efforts_by_project = project_positions.group("employees.id", "costgroups.number", "project_id").sum("project_efforts.value")
+    self.effort_minutes_weighted = weighted_effort_minutes
+  end
+
+  def weighted_effort_minutes
+    effort_minutes = {}
+    efforts_by_project.each do |(employee_id, cost_group_id, project_id), minutes|
+      project = projects.find { |project| project.id == project_id }
+
+      total_weight = 0.0
+
+      project.project_costgroup_distributions.each do |d|
+        total_weight += d.weight
+      end
+
+      if total_weight == 0
+        total_weight = 1.0
+      end
+
+      weight = project.project_costgroup_distributions.find do |cg|
+        cg.costgroup_number == cost_group_id
+      end.weight/total_weight
+
+      effort_minutes[[employee_id, cost_group_id]] ||= 0
+      effort_minutes[[employee_id, cost_group_id]] += minutes * weight
+    end
+    effort_minutes
   end
 
   def effort
     @effort = {}
-    effort_minutes.each do |(employee_id, cost_group_id), minutes|
+
+    effort_minutes_weighted.each do |(employee_id, cost_group_id), minutes|
       @effort[employee_id] ||= {}
       @effort[employee_id][cost_group_id] = minutes
     end
@@ -47,8 +77,14 @@ class CostGroupReportService
   end
 
   def footer
-    ["Total"] + cost_groups.map do |employee|
-      ((effort_minutes_by_cost_group[employee.id] || 0.0) / 60.0).round 2
+    effort_minutes_by_cost_group = {}
+    effort_minutes_weighted.each do |(employee_id, cost_group_id), minutes|
+      effort_minutes_by_cost_group[cost_group_id] ||= 0
+      effort_minutes_by_cost_group[cost_group_id] += minutes
+    end
+
+    ["Total"] + cost_groups.map do |cost_group|
+      ((effort_minutes_by_cost_group[cost_group.id] || 0.0) / 60.0).round 2
     end
   end
 
