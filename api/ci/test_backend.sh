b2e7798ae3d9e35c1de4a3e738d13dd569befc0d
Daniel Latzer <latzer.daniel@gmail.com>
2020-06-11 16:06:52 +0200

    Remove legacy php api

--- api/ci/test_backend.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -vue
-php artisan db:create
-php artisan migrate:fresh -q
-vendor/phpunit/phpunit/phpunit --order-by=random --coverage-clover=coverage.xml
-if [ $? == 0 ]; then
-    bash <(curl -s https://codecov.io/bash)
-fi
