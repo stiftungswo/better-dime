38f3bef5dfc90adee0c3c49ff7634ce7d0336009
Daniel Latzer <latzer.daniel@gmail.com>
2020-06-22 14:38:54 +0200

    Fix add_addresses_to_employees migration to work with fresh database

--- apir/db/migrate/20200526122928_add_addresses_to_employees.rb
+++ apir/db/migrate/20200526122928_add_addresses_to_employees.rb
@@ -1,7 +1,8 @@
 class AddAddressesToEmployees < ActiveRecord::Migration[6.0]
   def change
     change_column_null :addresses, :customer_id, true
-    add_column :addresses, :employee_id, :integer, unsigned: true, null: true
-    add_foreign_key :addresses, :employees
+    change_table :addresses do |t|
+      t.references :employee_id, foreign_key: { to_table: :employees}, null: true
+    end
   end
 end
