049ba036ac90ea4ebf8ea00a1d6e741326df69d6
JanicB <janic.berger@gmail.com>
2021-05-14 12:44:06 +0200

    Migration fix

--- apir/db/migrate/20200526122928_add_addresses_to_employees.rb
+++ apir/db/migrate/20200526122928_add_addresses_to_employees.rb
@@ -2,7 +2,7 @@ class AddAddressesToEmployees < ActiveRecord::Migration[6.0]
   def change
     change_column_null :addresses, :customer_id, true
     change_table :addresses do |t|
-      t.references :employee_id, foreign_key: { to_table: :employees}, null: true
+      t.references :employee, foreign_key: { to_table: :employees}, null: true
     end
   end
 end
--- apir/db/migrate/20200625102546_employee_first_vactaion_takeover_default.rb
+++ apir/db/migrate/20200625102546_employee_first_vactaion_takeover_default.rb
@@ -1,5 +1,5 @@
 class EmployeeFirstVactaionTakeoverDefault < ActiveRecord::Migration[6.0]
   def change
-    change_column_default :employees, :first_vacation_takeover, 0
+    change_column_default :employees, :first_vacation_takeover, from: 0, to: 0
   end
 end
