7b4061b884619c0eb732c682ad587f8fcaba8c63
jalthaus <jalthaus@student.ethz.ch>
2021-06-17 14:44:34 +0200

    create project from offer update

--- apir/app/services/project_creator.rb
+++ apir/app/services/project_creator.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ProjectCreator
-  def self.create_project_from_offer(offer)
+  def self.create_project_from_offer(offer, costgroup, category)
     project = Project.new
     project.offer = offer
     project.name = offer.name
@@ -11,12 +11,30 @@ class ProjectCreator
     project.rate_group = offer.rate_group
     project.fixed_price = offer.fixed_price
     project.description = offer.short_description
+    project.project_costgroup_distributions = create_costgroups(project, costgroup)
+    project.project_category_distributions = create_categories(project, category)
 
     project.project_positions = create_positions_from_offer(project, offer)
     project
   end
 
   private
+
+  def self.create_categories(project, category)
+    project_category = ProjectCategoryDistribution.new
+    project_category.project = project
+    project_category.project_category = ProjectCategory.find_by_id(category)
+    project_category.weight = 100
+    [project_category]
+  end
+
+  def self.create_costgroups(project, costgroup)
+    project_costgroup = ProjectCostgroupDistribution.new
+    project_costgroup.project = project
+    project_costgroup.costgroup = Costgroup.find_by_number(costgroup)
+    project_costgroup.weight = 100
+    [project_costgroup]
+  end
 
   def self.create_positions_from_offer(project, offer)
     offer.offer_positions.map do |position|
---
+++ frontend/src/views/offers/AddCCDialog.tsx
@@ -0,0 +1,72 @@
+import {FormikProps} from 'formik';
+import {inject, observer} from 'mobx-react';
+import * as React from 'react';
+import * as yup from 'yup';
+import CostgroupSelect from '../../form/entitySelect/CostgroupSelect';
+import {ProjectCategorySelect} from '../../form/entitySelect/ProjectCategorySelect';
+import {ProjectSelect} from '../../form/entitySelect/ProjectSelect';
+import {DimeField} from '../../form/fields/formik';
+import {FormDialog} from '../../form/FormDialog';
+import {OfferStore} from '../../stores/offerStore';
+import {ProjectStore} from '../../stores/projectStore';
+import {TimetrackFilterStore} from '../../stores/timetrackFilterStore';
+import {localizeSchema, nullableNumber, selector} from '../../utilities/validation';
+
+const schema = localizeSchema(() =>
+  yup.object({
+    costgroup: selector(),
+    category: selector(),
+  }),
+);
+
+const template = {
+  costgroup: '',
+  category: '',
+};
+
+type Values = typeof template;
+
+interface Props {
+  offerStore?: OfferStore;
+  projectStore?: ProjectStore;
+  onClose: () => void;
+  onConfirm: (costgroup: number, category: number) => void;
+}
+
+@inject('offerStore', 'projectStore')
+@observer
+export default class AddCCDialog extends React.Component<Props> {
+  handleSubmit = async (formValues: Values) => {
+    const values = schema.cast(formValues);
+    this.props.onConfirm(values.costgroup, values.category);
+    this.props.onClose();
+  }
+
+  render() {
+
+    return (
+      <FormDialog
+        open
+        onClose={this.props.onClose}
+        title={'Kostenstelle und Tätigkeitsbereich hinzufügen'}
+        initialValues={{
+          costgroup: '',
+          category: '',
+        }}
+        validationSchema={schema}
+        onSubmit={this.handleSubmit}
+        confirmText={'Projekt erstellen'}
+        render={(formikProps: FormikProps<Values>) => (
+          <>
+            <p>
+              Bevor ein Projekt erstellt werden kann, muss zuerst eine Kostenstelle und ein Tätigkeitsbereich
+              angegeben werden.
+            </p>
+            <DimeField component={CostgroupSelect} name={'costgroup'} label={'Kostenstelle'} />
+            <DimeField component={ProjectCategorySelect} name="category" label={'Tätigkeitsbereich'} />
+          </>
+        )}
+      />
+    );
+  }
+}
