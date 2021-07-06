577926f5211dc48864d9b11004a6952d57d74c77
jalthaus <jalthaus@student.ethz.ch>
2021-07-06 17:32:33 +0200

    Db Seed update

--- apir/db/seeds.rb
+++ apir/db/seeds.rb
@@ -6,8 +6,8 @@ uwt_group = EmployeeGroup.create! name: "UWT Teilnehmer"
 
 Employee.create!(
   [
-    { email: "zivi@example.com", first_name: "Zivi", last_name: "Muster", password: "123456", holidays_per_year: 20, employee_group: zivi_group },
-    { email: "employee@example.com", first_name: "Mitarbeiter", last_name: "Muster", password: "123456", holidays_per_year: 20, employee_group: employees_group },
-    { email: "uwt@example.com", first_name: "UWT", last_name: "Muster", password: "123456", holidays_per_year: 20, employee_group: uwt_group }
+    { email: "zivi@example.com", first_name: "Zivi", last_name: "Muster", password: "123456", holidays_per_year: 20, employee_group: zivi_group, is_admin: 1, can_login: 1 },
+    { email: "employee@example.com", first_name: "Mitarbeiter", last_name: "Muster", password: "123456", holidays_per_year: 20, employee_group: employees_group, can_login: 1 },
+    { email: "uwt@example.com", first_name: "UWT", last_name: "Muster", password: "123456", holidays_per_year: 20, employee_group: uwt_group, can_login: 1 }
   ]
 )
