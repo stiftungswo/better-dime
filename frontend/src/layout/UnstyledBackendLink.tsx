7ffc472e16d8ad5ad51a6b6e7bb59ae36bf93140
dacin21 <dacin21@users.noreply.github.com>
2022-01-14 16:14:09 +0100

    style fix, take 2.

--- frontend/src/layout/UnstyledBackendLink.tsx
+++ frontend/src/layout/UnstyledBackendLink.tsx
@@ -3,18 +3,17 @@ import * as React from 'react';
 
 interface Props {
   url: string;
-  showUrl?: boolean;
   children: React.ReactNode;
 }
 // use UnstyledBackendLink for relative frontend links,
 // and use UnstyledBackendLink for links to the backend, e.g. for PDF generation.
-export default function UnstyledBackendLink({url, showUrl, children}: Props) {
+export default function UnstyledBackendLink({url, children}: Props) {
   const theme = useTheme();
   return (
     <a
-      style={{ textDecoration: 'none', color: theme.palette.text.primary }}
+      style={{ textDecoration: 'none', color: 'inherit' }}
       href={url}
-      target={showUrl ? undefined : '_blank'}
+      target={'_blank'}
     >
       {children}
     </a>
