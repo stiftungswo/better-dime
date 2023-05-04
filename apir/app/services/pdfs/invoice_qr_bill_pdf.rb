cb7ce06cf412881d56bdc7af1be3613a902ebd4c
Simone Fuchs <sf@stiftungswo.ch>
2023-05-04 11:37:47 +0200

    Correct QR-Code. Line 2 can have 16 chars max.

--- apir/app/services/pdfs/invoice_qr_bill_pdf.rb
+++ apir/app/services/pdfs/invoice_qr_bill_pdf.rb
@@ -68,12 +68,12 @@ module Pdfs
       params[:bill_params][:currency]                         = "CHF"
       params[:bill_params][:debtor][:address][:type]          = "S"
       if @invoice.customer.company
-        params[:bill_params][:debtor][:address][:name]          = @invoice.customer.company.name
-        params[:bill_params][:debtor][:address][:line1]         = @invoice.customer.full_name
+        params[:bill_params][:debtor][:address][:name]          = @invoice.customer.company.name + ", " + @invoice.customer.full_name
+        #params[:bill_params][:debtor][:address][:line1]         = @invoice.customer.full_name
       else
         params[:bill_params][:debtor][:address][:name]          = @invoice.customer.full_name
       end
-      params[:bill_params][:debtor][:address][:line2]         = @invoice.address.street
+      params[:bill_params][:debtor][:address][:line1]         = @invoice.address.street
       params[:bill_params][:debtor][:address][:postal_code]   = @invoice.address.zip.to_s
       params[:bill_params][:debtor][:address][:town]          = @invoice.address.city
       params[:bill_params][:debtor][:address][:country]       = get_country_abbr(@invoice.address.country)
