a09486c0a6ef2b39837a365c94582e658ca35d00
Philipp Fehr <philipp@thefehr.ch>
2026-08-26 11:58:34 +0200

    Revert "Add eager_load spec; fix ApplicationJob commented out"
    This reverts commit 4225b8f7245e14ee127319371d16427570d31395.

--- apir/spec/application_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require "rails_helper"
-
-RSpec.describe "Application" do
-  it "eager loads without errors" do
-    expect { Rails.application.eager_load! }.not_to raise_error
-  end
-end
